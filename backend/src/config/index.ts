7866cb68ec9fab398b3d0f5298a9604712d6caa5
YaroslavHunt <yaroslavohotskiy@gamil.com>
2025-01-20 14:13:43 +0200

    Enable CORS

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -3,10 +3,12 @@ import dbConfig from './database/db.config';
 import jwtConfig from './token/jwt.config';
 import adminConfig from './admin/admin.config';
 import { MainConfig } from './config.types';
+import securityConfig from './security/security.config';
 
 export default (): MainConfig => ({
 	app: appConfig(),
 	database: dbConfig(),
 	jwt: jwtConfig(),
 	admin: adminConfig(),
+	security: securityConfig(),
 });
