61d202340fc93a758e31d93c8e769c1f83ff1f8f
YaroslavHunt <yaroslavohotskiy@gamil.com>
2025-02-19 01:31:26 +0200

    m

--- backend/src/mail/mail.module.ts
+++ backend/src/mail/mail.module.ts
@@ -4,11 +4,11 @@ import { MailerModule} from '@nestjs-modules/mailer'
 import { ConfigService } from '@nestjs/config'
 
 @Module({
-  imports: [MailerModule.forRootAsync({
-    imports: [ConfigService],
-    useFactory: getMailerConfig,
-    inject: [ConfigService]
-  })],
+  // imports: [MailerModule.forRootAsync({
+  //   imports: [ConfigService],
+  //   useFactory: getMailerConfig,
+  //   inject: [ConfigService]
+  // })],
   providers: [MailService],
 })
 export class MailModule {}
